ae973446ff1675c65b56fe2859ce2ff867527802
Chiratla Yuva Sai Manikanta <167677012+chiratla-yuva@users.noreply.github.com>
2025-03-21 19:04:43 +0530

    Update HistoricalData.jsx

--- client/src/pages/HistoricalData.jsx
+++ client/src/pages/HistoricalData.jsx
@@ -8,7 +8,7 @@ const HistoricalData = () => {
     useEffect(() => {
         const fetchHistoricalData = async () => {
             try {
-                const response = await axios.get("http://localhost:4000/api/sensors/historical");
+                const response = await axios.get("https://water-quality-backend-v5h7.onrender.com/api/sensors/historical");
                 if (response.data.success) {
                     setHistoricalData(response.data.data);
                 }
